285e263ba622ae9c21c4ff13aaaa7576e3fc9aa7
iomezk <9927685+iomezk@users.noreply.github.com>
2023-09-27 12:33:04 +0000

    run-xdelta3.sh: add mksquashfs

--- run-xdelta3.sh
+++ run-xdelta3.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
+set -e
 
+SNAP_NAME=firefox
 SNAP_ID=3wdHCAVyZEmYsCMFDE9qt92UV8rC8Wdk
 REV1=3131
 REV2=3166
 COMP=zstd
+LVL=22
 
-ln -sf "${SNAP_ID}_${REV1}.${COMP}.snap" "${SNAP_ID}_${REV1}.snap"
-ln -sf "${SNAP_ID}_${REV2}.${COMP}.snap" "${SNAP_ID}_${REV2}.snap"
+mksfs() {
+  # SOURCE_DATE_EPOCH=946684800
+  [ -e "${SNAP_ID}_${REV}.${COMP}.snap" ] || mksquashfs \
+   "/snap/${SNAP_NAME}/${REV}/" "${SNAP_ID}_${REV}.${COMP}.snap" -quiet \
+   -noappend -comp "$COMP" -no-fragments -no-progress -all-root -no-xattrs "$@"
+  ln -sf "${SNAP_ID}_${REV}.${COMP}.snap" "${SNAP_ID}_${REV}.snap"
+}
+if [ "x$LVL" = x- ]; then
+  set --
+else
+  set -- -Xcompression-level "$LVL"
+fi
+REV="$REV1" mksfs "$@"
+REV="$REV2" mksfs "$@"
 
 # no -A, as snapcraft.io does
-xdelta3 -e -f -s "${SNAP_ID}_${REV1}.snap" "${SNAP_ID}_${REV2}.snap" "firefox_${REV2}.snap.xdelta3-${REV1}-to-${REV2}-${COMP}"
+xdelta3 -e -f -s "${SNAP_ID}_${REV1}.snap" "${SNAP_ID}_${REV2}.snap" \
+ "${SNAP_NAME}_${REV2}.snap.xdelta3-${REV1}-to-${REV2}-${COMP}"
