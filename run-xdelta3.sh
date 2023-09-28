7dbf849aef394ef5e4c13fe821726b5eb93143a9
iomezk <9927685+iomezk@users.noreply.github.com>
2023-09-28 01:09:15 +0000

    run-xdelta3.sh,README.md: add non-compressed squashfs

--- run-xdelta3.sh
+++ run-xdelta3.sh
@@ -5,21 +5,23 @@ SNAP_NAME=firefox
 SNAP_ID=3wdHCAVyZEmYsCMFDE9qt92UV8rC8Wdk
 REV1=3131
 REV2=3166
-COMP=zstd
-LVL=22
+COMP=zstd LVL=22
 
 mksfs() {
   # SOURCE_DATE_EPOCH=946684800
   [ -e "${SNAP_ID}_${REV}.${COMP}.snap" ] || mksquashfs \
-   "/snap/${SNAP_NAME}/${REV}/" "${SNAP_ID}_${REV}.${COMP}.snap" -quiet \
-   -noappend -comp "$COMP" -no-fragments -no-progress -all-root -no-xattrs "$@"
-  ln -sf "${SNAP_ID}_${REV}.${COMP}.snap" "${SNAP_ID}_${REV}.snap"
+   "/snap/${SNAP_NAME}/${REV}/" "${SNAP_NAME}_${REV}.${COMP}.snap" -quiet \
+   -noappend -no-fragments -no-progress -all-root -no-xattrs "$@"
+  ln -sf "${SNAP_NAME}_${REV}.${COMP}.snap" "${SNAP_ID}_${REV}.snap"
 }
-if [ "x$LVL" = x- ]; then
-  set --
-else
-  set -- -Xcompression-level "$LVL"
-fi
+set --
+case "$COMP" in
+  no) set -- "$@" -comp xz -noI -noD ;;
+  *)  set -- "$@" -comp "$COMP"
+esac
+case "$LVL" in
+  [0-9]*) set -- "$@" -Xcompression-level "$LVL"
+esac
 REV="$REV1" mksfs "$@"
 REV="$REV2" mksfs "$@"
 
